21c6d3e4328611d493cce114cdcefc8dd5f75d84
Chris McArthur <prince.chrismc@gmail.com>
2018-12-14 21:49:47 -0500

    removing compile option for catch

--- tests/main.cpp
+++ tests/main.cpp
@@ -25,7 +25,6 @@ SOFTWARE.
 */
 
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
-#define CATCH_CONFIG_NO_CPP17_UNCAUGHT_EXCEPTIONS // Not supported by Clang 6.0
 #include "catch2/catch.hpp"
 
 #include "PassiveSocket.h"
