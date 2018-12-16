672b4d54c703fad6248064c42c56f546c244ce12
Chris McArthur <prince.chrismc@gmail.com>
2018-12-16 14:58:18 -0500

    correcting windows build
    error with header includes

--- tests/main.cpp
+++ tests/main.cpp
@@ -25,8 +25,8 @@ SOFTWARE.
 */
 
 #define CATCH_CONFIG_MAIN   // This tells Catch to provide a main() - only do this in one cpp file
-#include "PassiveSocket.h"
 #include "catch2/catch.hpp"
+#include "PassiveSocket.h"
 
 #include <future>
 #include <string_view>
