782e8b6096cb67a2e055bf99ffaf1b644bda28f9
Chris McArthur <prince.chrismc@gmail.com>
2019-04-21 09:23:38 -0400

    completed setup of cppcheck command

--- cmake/Simple-Socket-Linting.cmake
+++ cmake/Simple-Socket-Linting.cmake
@@ -4,28 +4,32 @@ find_program(CLANG_TIDY_EXE
              DOC "Path to clang-tidy executable")
 
 if(CLANG_TIDY_EXE)
-  message(STATUS "clang-tidy found: ${CLANG_TIDY_EXE}")
-  set(
-  CMAKE_CXX_CLANG_TIDY
-  "${CLANG_TIDY_EXE};-p=${CMAKE_CURRENT_BINARY_DIR};"
-  )
+  message(AUTHOR_WARNING "clang-tidy found: ${CLANG_TIDY_EXE}")
+  set(CMAKE_CXX_CLANG_TIDY "${CLANG_TIDY_EXE};-p=${CMAKE_CURRENT_BINARY_DIR};")
   # set(CXX_CLANG_TIDY ${CMAKE_CXX_CLANG_TIDY} CACHE STRING "" FORCE)
 else()
   message(AUTHOR_WARNING "clang-tidy not found!")
   set(CMAKE_CXX_CLANG_TIDY "" CACHE STRING "" FORCE) # delete it
 endif()
 
-add_custom_target(clangformat
-                  COMMAND ${CLANG_TIDY_EXE} -p=${CMAKE_CURRENT_BINARY_DIR}
-                          -checks=* -header-filter=${CMAKE_SOURCE_DIR}
-                          ${SIMPLE_SOCKET_SOURCES})
-add_custom_target(cppcheck
-                  COMMAND "cppcheck" "--enable=all"
-                          "--project=compile_commands.json" "--xml"
-                          "2> report.xml"
-                  COMMAND "cppcheck-htmlreport" "--file=report.xml"
-                          "--report-dir=report" "--source-dir=..")
+# Enable cppcheck
+find_program(CPPCHECK_EXE NAMES "cppcheck" DOC "Path to cppcheck executable")
 
-# set(CMAKE_CXX_CPPCHECK "cppcheck;--enable=all;--project=compile_commands.json")
-# set(CMAKE_CXX_CPPCHECK "")
-# set(CXX_CPPCHECK ${CMAKE_CXX_CPPCHECK} CACHE STRING "" FORCE)
+if(CPPCHECK_EXE)
+  message(AUTHOR_WARNING "cppcheck found: ${CPPCHECK_EXE}")
+  add_custom_target(cppcheck
+                    COMMAND "${CPPCHECK_EXE}" "-j4" "--enable=all" "--inconclusive"
+                            "--suppress=*:*catch.hpp" "--suppress=compareBoolExpressionWithInt:*tests/*.cpp"
+                            "--project=compile_commands.json" "--xml"
+                            "--output-file=report.xml"
+                    COMMAND "cppcheck-htmlreport" "--file=report.xml"
+                            "--report-dir=report" "--source-dir=..")
+else()
+  message(AUTHOR_WARNING "cppcheck not found!")
+  # set(CXX_CPPCHECK "" CACHE STRING "" FORCE)
+endif()
+
+# set(CMAKE_CXX_CPPCHECK
+# "cppcheck;--enable=all;--project=compile_commands.json")
+# set(CMAKE_CXX_CPPCHECK "") set(CXX_CPPCHECK ${CMAKE_CXX_CPPCHECK} CACHE STRING
+# "" FORCE)
