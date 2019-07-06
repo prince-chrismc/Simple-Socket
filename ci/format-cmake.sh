6f61bab607e660c76bd01919e365a0a4a17b38b8
Christopher McArthur <prince.chrismc@gmail.com>
2019-07-06 12:28:27 -0400

    moving cmake-format to a script

---
+++ ci/format-cmake.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+usage()
+{
+   echo "Usage: $0 [dir]\n"
+   echo "  Used to call the cmake-format on the various 'CMakeLists.txt' located under 'dir'\n"
+}
+
+if [ "$#" -ne 1 ]; then
+  usage
+  exit 1
+fi
+
+if [ ! -d "$1" ]; then
+  >&2 echo "$1 is not a directory"
+  usage
+  exit 1
+fi
+
+if [ ! -x $(which "cmake-format") ]; then
+  >&2 echo "ERROR: cmake-format does not exist or is not executable!"
+  exit 1
+fi
+
+for file in $(find $1 -name "*CMakeLists.txt")
+do
+   echo "Formatting $file..."
+   cmake-format "$file" > "$file.format"
+   rm "$file"
+   mv "$file.format" "$file"
+done
+
+exit 0
